7d7f0c8b2eaa42c15d0f35d011fc44ae9441b473
Rohan Marya <rohanmarya1507@gmail.com>
2024-01-02 00:04:16 +0530

    Displayed NewsItems from state by looping through an array in jsx

--- src/components/News.js
+++ src/components/News.js
@@ -25,24 +25,24 @@ export class News extends Component {
       "content": "Manchester United explained why four journalists were banned from an Erik ten Hag press conference on Tuesday.\r\nSky Sports chief reporter Kaveh Solhekhol, the Manchester Evening News' Samuel Luckhurs… [+820 chars]"
     },
     {
-      "source": { "id": "bleacher-report", "name": "Bleacher Report" },
+      "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
       "author": null,
-      "title": "New Micah Parsons Show ",
-      "description": "Fan easier, fan faster and fan better with Bleacher Report. Keep up with the latest storylines, expert analysis, highlights and scores for all your favorite sports.",
-      "url": "https://bleacherreport.com/videos/490566-the-edge-w-micah-parsons-ep-11-vod",
-      "urlToImage": null,
-      "publishedAt": "2023-11-27T20:37:24.6381564Z",
-      "content": null
+      "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
+      "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
+      "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
+      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
+      "publishedAt": "2020-04-27T11:41:47Z",
+      "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
     },
     {
-      "source": { "id": "bleacher-report", "name": "Bleacher Report" },
+      "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
       "author": null,
-      "title": " Mikal Bridges Interview ",
-      "description": "Fan easier, fan faster and fan better with Bleacher Report. Keep up with the latest storylines, expert analysis, highlights and scores for all your favorite sports.",
-      "url": "https://bleacherreport.com/videos/491103-taylor-rooks-x-mikal-bridges-vod",
-      "urlToImage": null,
-      "publishedAt": "2023-11-27T20:37:24.3882176Z",
-      "content": "Nets star sits down with Taylor Rooks for exclusive convo."
+      "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+      "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+      "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+      "publishedAt": "2020-03-30T15:26:05Z",
+      "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
     }
   ]
 
@@ -60,12 +60,15 @@ export class News extends Component {
       <div className="container my-3">
 
         <h2>NewsMonkey - Top Headlines </h2>
+        {/* {this.state.articles.map((element)=>{console.log(element)})}  --displays the objects of article */}
         <div className="row">
-          <div className="col-md-4">
-            <NewsItem title="Mytitle" description="Mydesc" 
-            imageUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg"
-            newsUrl="TODO"/>
-          </div>
+          {this.state.articles.map((element)=>{
+            return <div className="col-md-4" key={element.url}>
+                      <NewsItem title={element.title.length>=45?element.title.slice(0, 45):element.title} 
+                      description={element.description.length>=60?element.description.slice(0, 60):element.description} 
+                      imageUrl={element.urlToImage} newsUrl={element.url}/>
+                    </div>
+          })}
         </div>
 
       </div>

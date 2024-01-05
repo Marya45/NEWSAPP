7a13551c3d72f9c2fee439e6bfd1b39f138fa58d
Rohan Marya <rohanmarya1507@gmail.com>
2024-01-06 00:03:35 +0530

    Changed class based components to function based and a sticky navbar

--- src/components/News.js
+++ src/components/News.js
@@ -1,102 +1,75 @@
-import React, { Component } from 'react';
+import React,{useEffect,useState} from 'react';
 import NewsItem from './NewsItem';
 import Spinner from './Spinner';
 import PropTypes from 'prop-types';
 import InfiniteScroll from "react-infinite-scroll-component";
 
-export class News extends Component {
+const News = (props)=> {
+  const [articles,setArticles] = useState([]);
+  const [loading,setLoading] = useState(true);
+  const [page,setPage] = useState(1);
+  const [totalResults,setTotalResults] = useState(0);
 
-  static defaultProps = {
-    country: 'in',
-    pageSize: 8,
-    category: 'general'
-  }
-
-  static propTypes = {
-    country: PropTypes.string,
-    pageSize: PropTypes.number,
-    category: PropTypes.string,
-  }
-
-  capitalizefirstletter = (string)=>{
+  const capitalizefirstletter = (string)=>{
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
-  constructor(props){
-    super(props);
-    // console.log("Hello i am a contructor from new component");
-    this.state = {
-      articles: [],
-      loading: false,
-      page: 1,
-      totalResults: 0
-    }
-    document.title = `NewsMonkey - ${this.capitalizefirstletter(this.props.category)}`;
-  }
-
-  async updateNews(){
-    this.props.setProgress(10);
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    this.setState({loading: true});
+  const updateNews = async()=>{
+    props.setProgress(10);
+    let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+    setLoading(true);
     let data = await fetch(url);
-    this.props.setProgress(30);
+    props.setProgress(30);
     let parsedData = await data.json();
-    this.props.setProgress(70);
-    // console.log(parsedData);
-    this.setState({articles: parsedData.articles , 
-      totalResults: parsedData.totalResults , 
-      loading: false
-    });
-    this.props.setProgress(100);
+    props.setProgress(70);
+
+    setArticles(parsedData.articles);
+    setTotalResults(parsedData.totalResults);
+    setLoading(false);
+    props.setProgress(100);
   }
 
-//run after the render() 
-  async componentDidMount(){
-    this.updateNews();
-  }
+  useEffect(()=>{
+    document.title = `NewsMonkey - ${capitalizefirstletter(props.category)}`;
+    updateNews();
+  },[])
 
-  handleNextClick = async()=>{
-    this.setState({page: this.state.page + 1});
-    this.updateNews();
-  }
+  // const handleNextClick = async()=>{
+  //   setPage(page+1);
+  //   updateNews();
+  // }
   
-  handlePreviousClick = async()=>{
-    this.setState({page: this.state.page - 1});
-    this.updateNews();
-  }
+  // const handlePreviousClick = async()=>{
+  //   setPage(page-1);
+  //   updateNews();
+  // }
 
-  fetchMoreData = async() => {
-    this.setState({page: this.state.page + 1});
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    this.setState({loading: true});
+  const fetchMoreData = async() => {
+    setPage(page+1);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+    // setLoading(true);
     let data = await fetch(url);
     let parsedData = await data.json();
-    // console.log(parsedData);
-    this.setState({articles: this.state.articles.concat(parsedData.articles) , 
-      totalResults: parsedData.totalResults , 
-      loading: false
-    });
+    setArticles(articles.concat(parsedData.articles));
+    setTotalResults(parsedData.totalResults);
+    // setLoading(false);
   };
 
-  render() {
-    // console.log("render");
     return (
       <>
-        <h1 className="text-center" style={{margin: '35px 0px'}} >NewsMonkey - Top {this.capitalizefirstletter(this.props.category)} Headlines </h1>
-        {/* {this.state.loading && <Spinner/>} */}
-
-        {/* {this.state.articles.map((element)=>{console.log(element)})}  --displays the objects of article */}
+        <h1 className="text-center" style={{margin: '35px 0px',marginTop: '80px'}} >NewsMonkey - Top {capitalizefirstletter(props.category)} Headlines </h1>
+        {loading && <Spinner/>}
 
         <InfiniteScroll
-          dataLength={this.state.articles.length}
-          next={this.fetchMoreData}
-          hasMore={this.state.articles.length <= this.state.totalResults}
+          dataLength={articles.length}
+          next={fetchMoreData}
+          hasMore={articles.length <= totalResults}
           loader={<Spinner/>}
         >
           <div className="container">
 
           <div className="row">
-            {this.state.articles.map((element,index)=>{
+            {articles.map((element,index)=>{
               return <div className="col-md-4" key={index}>
                         <NewsItem title={element.title?(element.title.length>=45?element.title.slice(0, 45):element.title):""} 
                         description={element.description?(element.description.length>=60?element.description.slice(0, 60):element.description):""} 
@@ -110,7 +83,19 @@ export class News extends Component {
 
       </>
     )
-  }
 }
+
+News.defaultProps = {
+  country: 'in',
+  pageSize: 8,
+  category: 'general'
+}
+
+News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
+}
+
 
 export default News
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class Spinner extends Component {
-  render() {
+const Spinner = ()=>{
     return (
       <div className="container text-center my-3">
         <div className="spinner-border text-dark text-center" role="status">
         </div>
       </div>
     )
-  }
 }
 
 export default Spinner
